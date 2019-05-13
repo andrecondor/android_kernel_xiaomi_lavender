00ad8f28f58d9244685dedbad3cd3cf0085521a4
Tyler Nijmeh <tylernij@gmail.com>
2019-05-12 21:12:38 -0700

    kernel/sysctl: Place sched_child_runs_first in sched sysctl macro
    This should really be inside of the sysctl check, since it is a
    scheduler tunable.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>
    Signed-off-by: sohamxda7 <sensoham135@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -322,6 +322,13 @@ static struct ctl_table kern_table[] = {
 #endif
 #ifdef CONFIG_SCHED_DEBUG
 	{
+		.procname	= "sched_child_runs_first",
+		.data		= &sysctl_sched_child_runs_first,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},	
+	{
 		.procname	= "sched_cpu_schedtune_bias",
 		.data		= &sysctl_sched_cpu_schedtune_bias,
 		.maxlen		= sizeof(unsigned int),
